fb40054baba9a169c95569b1188dc7540d29ad89
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-06-18 22:20:11 +0200

    Add live display input update, storing num & oper.

--- javascrips.js
+++ javascrips.js
@@ -11,6 +11,21 @@ function divide(a,b) {
     return a / b
 }
 
-let number = 0;
+let number = '';
 let operator = '';
 let secondNumber = 0;
+
+let input = document.querySelector('#input')
+let numberButtons = document.querySelector('#numbers');
+let inputNumber = numberButtons.addEventListener('click', (e) => {
+    number += e.target.textContent;
+    input.textContent += e.target.textContent;
+})
+
+let operatorButtons = document.querySelector('#operators');
+operator = operatorButtons.addEventListener('click', (e) => {
+    input.textContent += e.target.textContent;
+    operator = e.target.textContent;
+    console.log(operator, number);
+})
+
