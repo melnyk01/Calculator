c32a08a2317ed1dc03521022861a833852c81904
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-06-19 19:46:26 +0200

    Calculates one expression, displaying the result

--- javascrips.js
+++ javascrips.js
@@ -1,31 +1,64 @@
-function add(a,b) {
-    return a + b
+function add(a, b) {
+    return +a + +b
 }
-function subtract(a,b) {
-    return a - b
+function subtract(a, b) {
+    return +a - +b
 }
-function multiply(a,b) {
-    return a * b
+function multiply(a, b) {
+    return +a * +b
 }
-function divide(a,b) {
-    return a / b
+function divide(a, b) {
+    return +a / +b
 }
 
 let number = '';
 let operator = '';
-let secondNumber = 0;
+let secondNumber = '';
 
+//Clear the input field
+let clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => input.textContent = '')
+
+//Getting first number
 let input = document.querySelector('#input')
 let numberButtons = document.querySelector('#numbers');
-let inputNumber = numberButtons.addEventListener('click', (e) => {
-    number += e.target.textContent;
+let inputNumber = numberButtons.addEventListener
+    ('click', (e) => {
     input.textContent += e.target.textContent;
+    number += e.target.textContent
 })
 
+//Getting operator
 let operatorButtons = document.querySelector('#operators');
-operator = operatorButtons.addEventListener('click', (e) => {
+operator = operatorButtons.addEventListener('click', (e)=> {
     input.textContent += e.target.textContent;
     operator = e.target.textContent;
-    console.log(operator, number);
-})
+    //Getting second number
+    let secondInputNumber = numberButtons.addEventListener
+    ('click', (e) => {
+    secondNumber += e.target.textContent;
+    console.log(number, secondNumber, operator)
+    })
+});
+// Calculates the input numbers
+let calculate = document.querySelector('button#calculate');
+calculate.addEventListener('click', () => {
+    number = number.slice(0, secondNumber.length)
+    let result = operate(number, secondNumber)
+    input.textContent = result;
+});
 
+function operate(a, b) {
+    if (operator == '+') {
+        return (add(number,secondNumber));
+    }
+    if (operator == '-') {
+        return (subtract(number,secondNumber));
+    }
+    if (operator == '*') {
+        return (multiply(number,secondNumber));
+    }
+    if (operator == '/') {
+        return (divide(number,secondNumber));
+    }
+}
